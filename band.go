3f86b62dc5e23c2d010d8a272589c9a690f7083d
Charles Perkins <chuck@kuracali.com>
2019-03-18 11:22:42 -0700

    add structures

--- band.go
+++ band.go
@@ -7,6 +7,49 @@ package band
 
 import "fmt"
 
+const (
+  FOUND = iota      // found a band of individuals
+  SPONSOR = iota    // sponsor the membership of an individual in a band
+  REJECT = iota     // reject the membership of an individual in a band
+  SUPPORT = iota    // support an individual for a role
+  OPPOSE = iota     // oppose an individual for a rola
+  SUSTAIN = iota    // sustain a role in a band
+  DETRACT = iota    // detract (oppose sustaining) a role in a band
+  AFFIRM = iota     // affirm a name for an individual in a band
+  DISPUTE = iota    // dispute (oppose affirming) a name for an individual in a band
+)
+
+type Band struct {
+  Name string
+  Hash []byte
+}
+
+type Self struct {
+  Privkey []byte
+  Pubkey []byte
+  Hash []byte
+  Name string
+}
+
+type Other struct {
+  Pubkey []byte
+  Hash []byte
+  Name string
+}
+
+type Claim struct {
+  Typ int
+  Key []byte
+  Val []byte
+}
+
+
+
+func (Band) GetName(debug bool){
+ fmt.Println( "GetName")
+
+}
+
 
 func Initialize(debug bool)  {
  fmt.Println( "initializing-internal-store")
